9cc02d83667f5ae243f373ca6b99685c6eb76d45
aaron1792 <aaron1792@gmail.com>
2024-05-15 15:34:05 +0100

    commit 4

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -115,6 +115,33 @@ board.map((_ , index) => {
 <Square isSelected= {turn === TURNS.O}>{TURNS.O}</Square>
 
  </section>
+ {winner !== null && (
+  <section className='winner'>
+    <div className='text'>
+      <h2>
+
+        {
+          winner === false ? 'Empate' : 'Gano'
+        }
+      </h2>
+
+      <header className='win'>
+        {winner && <Square>{winner}</Square>}
+
+
+      </header>
+      <footer>
+
+        <button>Empezar de nuevo</button>
+      </footer>
+
+
+    </div>
+
+
+
+  </section>
+ ) }
   </main>
   
   
