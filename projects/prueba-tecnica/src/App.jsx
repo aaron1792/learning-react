c0f96aa12e424a1e3598439a0b7b8689ccf707a4
aaron1792 <aaron1792@gmail.com>
2024-06-05 15:55:26 +0100

    hooks 2

--- projects/prueba-tecnica/src/App.jsx
+++ projects/prueba-tecnica/src/App.jsx
@@ -1,34 +1,9 @@
 import { useEffect, useState } from 'react';
 import { getRandomFact } from './logic/facts';
+import { useCatImage } from './hooks/useCatImage';
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact';
 //const CAT_ENDPOINT_IMAGE_URL =`https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true`
 const CAT_PREFIX_IMAGE_URL = 'https://cataas.com';
-
-function useCatImage({ fact }) {
-  const [imageUrl, setImageUrl] = useState();
-
-  //para recuperar la imagen cada vez que tenemos una cita nueva
-
-  useEffect(() => {
-    if (!fact) return;
-
-    const firstWord = fact.split(' ', 3).join(' ');
-    console.log(firstWord);
-
-    fetch(
-      `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true`
-    )
-      .then((res) => res.json())
-      .then((response) => {
-        console.log(response);
-
-        const { url } = response;
-
-        setImageUrl(`${url}`);
-      });
-  }, [fact]);
-  return { imageUrl };
-} // {imageUrl: 'https://...'}
 
 export function App() {
   const [fact, setFact] = useState();
---
+++ projects/prueba-tecnica/src/hooks/useCatImage.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+
+export function useCatImage({ fact }) {
+    const [imageUrl, setImageUrl] = useState();
+  
+    //para recuperar la imagen cada vez que tenemos una cita nueva
+  
+    useEffect(() => {
+      if (!fact) return;
+  
+      const firstWord = fact.split(' ', 3).join(' ');
+      console.log(firstWord);
+  
+      fetch(
+        `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true`
+      )
+        .then((res) => res.json())
+        .then((response) => {
+          console.log(response);
+  
+          const { url } = response;
+  
+          setImageUrl(`${url}`);
+        });
+    }, [fact]);
+    return { imageUrl };
+  } // {imageUrl: 'https://...'}
