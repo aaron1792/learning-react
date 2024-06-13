2a923094b6cb9562f123590c7fc6868273c2fa02
aaron1792 <aaron1792@gmail.com>
2024-06-13 15:48:56 +0100

    06-shopping-cart

---
+++ projects/06-shopping-cart/src/App.jsx
@@ -0,0 +1,33 @@
+import { products as initialProducs } from './mocks/products.json';
+
+import { Products } from './components/Products';
+import { useState } from 'react';
+import { Header } from './components/Header';
+
+function App() {
+  const [products] = useState(initialProducs);
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 0,
+  });
+
+  const FiltersProducts = (products) => {
+    return products.filter((product) => {
+      return (
+        product.price >= filters.minPrice &&
+        (filters.category === 'all' || product.category === filters.category)
+      );
+    });
+  };
+
+  const filteredProducts = FiltersProducts(products);
+
+  return (
+    <>
+      <Header />
+      <Products products={filteredProducts} />
+    </>
+  );
+}
+
+export default App;
---
+++ projects/06-shopping-cart/src/components/Products.jsx
@@ -0,0 +1,31 @@
+import './Products.css';
+
+import { AddToCartIcon } from './Icons';
+
+export function Products({ products }) {
+  return (
+    <main className="products">
+      <ul>
+        {products.slice(0, 20).map((product) => (
+          <li key={product.id}>
+            <img
+              src={product.thumbnail}
+              alt={product.title}
+            />
+
+            <div>
+              <strong>
+                {product.title} - ${product.price}
+              </strong>
+            </div>
+            <div>
+              <button>
+                <AddToCartIcon />
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
